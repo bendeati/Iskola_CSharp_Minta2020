e3f6a9ee84ef164f63acd436aa041b8a93dac64c
Bende Attila <47759047+bendeati@users.noreply.github.com>
2020-04-27 20:30:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanulok_konzolos
+{
+    class JelszoGeneralo
+    {
+        private Random Rnd;
+
+        public JelszoGeneralo(Random r)
+        {
+            Rnd = r;
+        }
+
+        public string Jelszó(int jelszóHossz)
+        {
+            string jelszó = "";
+            while (jelszó.Length < jelszóHossz)
+            {
+                char c = (char)Rnd.Next(48, 123);
+                if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z'))
+                {
+                    jelszó += c;
+                }
+            }
+            return jelszó;
+        }
+    }
+
+    class Program
+    {
+        static List<TanuloClass> tanLista = new List<TanuloClass>();
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("nevek.txt", Encoding.UTF8);
+            string sor = "";
+            while (!sr.EndOfStream)
+            {
+                sor = sr.ReadLine();
+                TanuloClass t = new TanuloClass(sor);
+                tanLista.Add(t);
+            }
+            sr.Close();
+
+            Console.WriteLine("3. feladat");
+            Console.WriteLine("Az iskolába járó tanulók: " + tanLista.Count);
+            Console.WriteLine($"Ennyien vannak: {tanLista.Count}");
+
+
+            Console.WriteLine("4. feladat");
+            int maxHossz = tanLista[0].nevHossz();
+            for (int i = 0; i < tanLista.Count; i++)
+            {
+                if (tanLista[i].nevHossz() > maxHossz)
+                {
+                    maxHossz = tanLista[i].nevHossz();
+                }
+            }
+            Console.WriteLine("A leghosszabb név: " + maxHossz);
+            for (int i = 0; i < tanLista.Count; i++)
+            {
+                if (tanLista[i].nevHossz() == maxHossz)
+                {
+                    Console.WriteLine(tanLista[i].Nev);
+                }
+            }
+
+            Console.WriteLine("5. feladat");
+            Console.WriteLine("Első tanuló: " + tanLista[0].Nev + ", " + tanLista[0].azonosito());
+            Console.WriteLine("Utolsó tanuló: " + tanLista[tanLista.Count-1].Nev + ", " 
+                + tanLista[tanLista.Count - 1].azonosito());
+
+            Console.WriteLine("6. feladat");
+            Console.Write("Kérek egy azonosítót: ");
+            string bekertAzon = Console.ReadLine();
+            int index = 0;
+            bool vanIlyen = false;
+            while (vanIlyen == false && index < tanLista.Count)
+            {
+                if (tanLista[index].azonosito() == bekertAzon)
+                {
+                    vanIlyen = true;
+                    Console.WriteLine(tanLista[index].Nev);
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            if (vanIlyen == false)
+            {
+                Console.WriteLine("Nincs ilyen tanuló!");
+            }
+
+            Console.WriteLine("7. feladat");
+            //Véletlen tanuló kiválasztása
+            Random r = new Random();
+            int randomIndex = r.Next(0,tanLista.Count);
+            TanuloClass randomTanulo = tanLista[randomIndex];
+            JelszoGeneralo j = new JelszoGeneralo(r);
+            Console.WriteLine("A tanuló: " + randomTanulo.Nev);
+            Console.WriteLine("A jelszó: " + j.Jelszó(8));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TanuloClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanulok_konzolos
+{
+    class TanuloClass
+    {
+        private int ev;
+        private string osztaly;
+        private string nev;
+
+        //konstruktor
+        public TanuloClass(string sor)
+        {
+            string[] d = sor.Split(';');
+            ev = Convert.ToInt32(d[0]);
+            osztaly = d[1];
+            nev = d[2];
+        }
+
+        //get és set
+        public int Ev { get => ev; set => ev = value; }
+        public string Osztaly { get => osztaly; set => osztaly = value; }
+        public string Nev { get => nev; set => nev = value; }
+
+        public int nevHossz()
+        {
+            //szóközök megszámolás
+            int szokozDB = 0;
+            for (int i = 0; i < nev.Length; i++)
+            {
+                if (nev[i] == ' ')
+                {
+                    szokozDB++;
+                }
+            }
+            return (nev.Length - szokozDB);
+        }
+
+        public string azonosito()
+        {
+            char elso = ev.ToString()[3];
+            string vnev = nev.Split(' ')[0].Substring(0, 3).ToLower();
+            string knev = nev.Split(' ')[1].Substring(0, 3).ToLower();
+            return (elso+osztaly+vnev+knev);
+        }
+    }
+}
